c84fa35ad8c30933db03dbaaa513cfb4b50dcc3c
Jacob Nickerson <jacobnickerson817@gmail.com>
2024-06-09 19:27:34 -0400

    board.hpp and board.cpp were completely redone, board is now represented as an array matrix of pointers, which point to blocks, the getBlock method was added to board and returns a pointer to a block given an x and y coordinate; the removeRow method was also added which calls deactivate on an entire row of the board given the y coordinate of the row to be removed; getBlock is implemented in board.cpp

--- include/board.hpp
+++ include/board.hpp
@@ -1,39 +1,46 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
-#include <vector>
-#include "block.hpp"
-#include "tetromino.hpp"
+#include <array>
 
+#include "block.hpp"
+
+static constexpr int BOARD_HEIGHT = 20;
+static constexpr int BOARD_WIDTH = 10;
 
 class Board {
     private:
-        int width;
-        int height;
-        std::vector<Block> matrix;
+        std::array<std::array<Block*, BOARD_WIDTH>, BOARD_HEIGHT> board_matrix = {};
 
     public:
         // Constructor
-        Board(int _width, int _height);
+        Board() {
+            for (int y = 0; y < BOARD_HEIGHT; y++) {
+                for (int x = 0; x < BOARD_WIDTH; x++) {
+                    board_matrix[y][x] = new Block(x,y);
+                }
+            }
+        }
 
-        // Clears one row from the board, board is 0 indexed
-        void clearRow(int y);
+        // Deconstructor (deleter, even)
+        ~Board() {
+            for (int y = 0; y < BOARD_HEIGHT; y++) {
+                for (int x = 0; x < BOARD_WIDTH; x++) {
+                    delete board_matrix[y][x];
+                }
+            }
+        }
 
-        // Spawns a tetromino (duh)
-        // Returns a bool, true if the tetromino is successfully spawned else false
-        bool spawnTetromino(Tetromino new_spawn);
+        // Get a pointer to the block at the position x,y
+        Block* getBlock(int x, int y); 
 
-        // Takes 0 indexed x,y coordinates (typically from a block) and converts to an int for the indexed position in 1D matrix
-        int getIndex(int x, int y);
-
-        
-
-        
-
-
-
-
-
+        // does what it says on the tin
+        // y is row index position
+        void removeRow(int y) {
+            for (int i=0; i < BOARD_WIDTH; i++) {
+                board_matrix[y][i]->deactivate();
+            }
+        }
 };
 
 #endif
--- src/board.cpp
+++ src/board.cpp
@@ -1,24 +1,6 @@
 #include "board.hpp"
 
 
-Board::Board(int x, int y) {
-    width = x;
-    height = y;
-    for (int j = 0; j < y; j++) {
-        for (int i = 0; i < x; i++) {
-            Block temp(i, j);
-            matrix.push_back(temp);
-        }
-    }
-}
-
-void Board::clearRow(int y) {
-    int start = y * width;
-    for (int i = y*width; i < (y+1)*width; i++) {
-        // clear board[i]
-    }
-}
-
-int Board::getIndex(int x, int y) {
-    return width*x+height*y;
-}
+Block* Board::getBlock(int x, int y) {
+    return board_matrix[y][x];
+};
